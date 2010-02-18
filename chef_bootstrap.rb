52dee7051973daf569d36483d46a5de80f406e7f
Matt Pearce <matt@pearce.com>
2010-02-18 09:12:15 +0800

    Update bootstrap script to install rsync and net/https on ubuntu 9.10.  Include sample commands to install boostrap script.

--- chef_bootstrap.rb
+++ chef_bootstrap.rb
@@ -7,6 +7,11 @@
 # config tools including ruby, rubygems, chef.  Run it from the /tmp dir on the remote machine since we don't
 # care if it is lost once we have run it once.  This script assumes that Ruby is already installed.
 
+#scp ~/.ssh/id_rsa.pub root@yourserver:.ssh/authorized_keys2
+#scp chef_bootstrap.rb root@yourserver:/tmp/
+#ssh root@yourserver
+#- aptitude -y install ruby;ruby /tmp/chef_bootstrap.rb
+
 # WARNING : This has only been tested on Ubuntu.  This only needs to be run to bootstrap a bare AMI.
 
 def cmd(cmd)
@@ -18,7 +23,7 @@ puts "Installing Chef"
 puts
 
 cmd "aptitude -y update"
-cmd "aptitude -y install irb ri rdoc libshadow-ruby1.8 ruby1.8-dev gcc g++ curl"
+cmd "aptitude -y install irb ri rdoc libshadow-ruby1.8 ruby1.8-dev gcc g++ curl rsync libopenssl-ruby1.8"
 
 cmd "curl -L 'http://rubyforge.org/frs/download.php/57643/rubygems-1.3.4.tgz' | tar xvzf -"
 cmd "cd rubygems* && ruby setup.rb --no-ri --no-rdoc"
